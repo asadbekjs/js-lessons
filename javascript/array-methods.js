68dd02e855c33da6c2a15352cbc53bb9d0ed661f
asadbekjs <asadbek_rahimov@mail.ru>
2026-01-14 16:37:44 +0500

    learn: array in JS

---
+++ javascript/array-methods.js
@@ -0,0 +1,9 @@
+// Array methods
+// 1. at()
+const cars = ["audi", "bmw", "porsche"];
+console.log(cars.at(1)); // "bmw"
+console.log(cars.at(4)); // undefined
+console.log(cars.at(-1)); // 'porsche'
+
+// 2. toString()
+console.log(cars.toString()); // 'audi,bmw,porsche'
--- javascript/array.js
+++ javascript/array.js
@@ -17,3 +17,32 @@ console.log(fruits[5]); // undefined, indeks mavjud emas
 // Element o'zgartirish
 fruits[1] = "Anor";
 console.log(fruits); // ["Olma", "Anor", "Apelsin"]
+
+const cars = new Array("bmw", "toyota", "chevrolet");
+console.log(cars.toString());
+
+const users = [
+  {
+    id: 1,
+    firstName: "Mardon",
+    email: "mardonjorayev@gmail.com",
+  },
+  {
+    id: 2,
+    firstName: "Yulduz",
+    email: "yulduzusmonova@gmail.com",
+  },
+  {
+    id: 3,
+    firstName: "Alex",
+    email: "alexsasha77@gmail.com",
+  },
+];
+// console.log(users[1]);
+for (let i = 0; i < users.length; i++) {
+  console.log(users[i].firstName + " " + users[i].email);
+}
+
+console.log(typeof users); // object
+console.log(Array.isArray(users)); // true
+console.log(Array.isArray({ name: "john", age: 36 })); // false
